b6a087154ca4f333e5a0836d697b7fc03f71b6d3
Richard Hanolt <rhanolt@gmail.com>
2020-05-10 02:58:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,5 +85,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+  :holidays_with_bbqs_hash = []
+  holiday_hash.each do |season, holiday|
+    holiday_each do |specific_holiday, supplies_and_decor|
+      if supplies_and_decor.include?("BBQ")
+        :holidays_with_bbqs_hash << supplies_and_decor
+      end
+    end
+  end
 end
